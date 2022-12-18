2663cf84311001da3cf75ef8e80aaf6ff4e245f0
abhiawatade <awatadeabhi@gmail.com>
2022-12-18 15:57:45 +0530

    user routes middleware updated

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const { connectDB } = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
 // middleware
 app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("Content"));
+
+app.use("api/v1/user", userRoutes);
 
 app.listen(port, () => {
   console.log("server listening on port");
