7ac2ba0ad1e9b61b195840b2f947c3fa605a17dd
abhiawatade <awatadeabhi@gmail.com>
2022-12-18 21:44:57 +0530

    signin route created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const {
   validateName,
@@ -48,10 +49,43 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.get("/acc", async (req, res) => {
-  return res.status(200).json({
-    message: `Welcome ${createdUser.name},Your account is successfully created`,
-  });
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (email.length === 0) {
+      return res.status(403).json({ err: "Please enter a valid email" });
+    }
+    if (password.length === 0) {
+      return res.status(403).json({ err: "Please enter a valid password" });
+    }
+
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (!existingUser) {
+      return res.status(403).json({ err: "User not found" });
+    }
+
+    const passwordMatched = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!passwordMatched) {
+      return res.status(403).json({ err: "email or password mismatch" });
+    }
+
+    const payload = { user: { id: existingUser.id } };
+    const bearerToken = await jwt.sign(payload, "SECRET MESSAGE", {
+      expiresIn: 36000,
+    });
+
+    res.cookie("t", bearerToken, { expire: new Date() + 9999 });
+
+    return res.status(200).json({ bearerToken: bearerToken });
+  } catch (error) {
+    console.log(">>>>>", error);
+    return res.status(500).send(error);
+  }
 });
 
 module.exports = router;
