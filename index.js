93ca90b914445d631b8035370fda69dae779cedc
abhiawatade <awatadeabhi@gmail.com>
2022-12-21 00:20:45 +0530

    typo fix

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 
 // middleware
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("Content"));
 
-app.use("api/v1/user", userRoutes);
-app.use("api/v1/product", productRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/product", productRoutes);
 
 app.listen(port, () => {
   console.log("server listening on port");
