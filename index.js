084c26033f326c7632b3b3da4398b8828e6e8488
abhiawatade <awatadeabhi@gmail.com>
2022-12-20 22:38:43 +0530

    product routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3001;
 const { connectDB } = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 // middleware
 app.use(express.json);
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("Content"));
 
 app.use("api/v1/user", userRoutes);
+app.use("api/v1/product", productRoutes);
 
 app.listen(port, () => {
   console.log("server listening on port");
